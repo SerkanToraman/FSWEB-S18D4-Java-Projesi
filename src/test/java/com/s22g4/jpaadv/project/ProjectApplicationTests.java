c0b801ba185f1a6a0bedc7445bc2212e6f05c61e
serkantoraman <serkan@github.com>
2023-09-01 16:10:44 +0300

    init commit

---
+++ src/test/java/com/s22g4/jpaadv/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s22g4.jpaadv.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
